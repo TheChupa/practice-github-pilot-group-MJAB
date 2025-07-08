4743f5a295ffbafdfb0bcccb2a7ae3089d649966
TheChupa <160878340+TheChupa@users.noreply.github.com>
2025-07-08 15:31:06 -0500

    Initial commit

---
+++ src/main/java/Jill_Addison_Ben_Michael/Group/Manager/GroupManagerApplication.java
@@ -0,0 +1,13 @@
+package Jill_Addison_Ben_Michael.Group.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Jill_Addison_Ben_Michael/Group/Manager/GroupManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package Jill_Addison_Ben_Michael.Group.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
